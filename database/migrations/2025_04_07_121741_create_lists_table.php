7dd47ae0d02b625247fb4e2ed4e245afb398847a
bevannstivens <bevannstivens2@gmail.com>
2025-04-07 19:37:24 +0700

    Creating database migration for users, lists, and tasks

---
+++ database/migrations/2025_04_07_121741_create_lists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['in_progress', 'done'])->default('in_progress');
+            $table->enum('priority', ['low', 'medium', 'high'])->default('low');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lists');
+    }
+};
